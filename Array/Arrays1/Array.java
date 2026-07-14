0a34c8b2f6b611dd3c3892d35eb23713e4a40809
Anadi <anirbanmondal53717@gmail.com>
2026-07-14 23:27:08 +0530

    Dutch flag algo

--- Array/Arrays1/Array.java
+++ Array/Arrays1/Array.java
@@ -18,6 +18,32 @@ public class Array {
         return max;
     }
 
+    //Sort arrays 0's 1's and 2's using Dutch flag algo
+    private void swap(int[]nums,int i,int j){
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+    }
+    public void sortColors(int[] nums) {
+        int mid =0,low=0, high=nums.length-1;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums, mid, low);
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums, mid, high);
+                high--;
+            }
+
+        }
+    }
+
     public static void main(String[] args) {
 
     }
