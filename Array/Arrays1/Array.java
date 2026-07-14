2c4b956651a8109d23760db59a0924664d069086
Anadi <anirbanmondal53717@gmail.com>
2026-07-14 20:28:44 +0530

    Kedans algo

---
+++ Array/Arrays1/Array.java
@@ -0,0 +1,24 @@
+public class Array {
+
+    //Kedans Algo
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>max){
+                max=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
