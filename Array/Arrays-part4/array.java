5c7792c36593ef7dad03f8c12b9a9eae71515a1d
Anadi <anirbanmondal53717@gmail.com>
2026-07-24 12:52:56 +0530

    Longest Consecutive Sequence in an Array

---
+++ Array/Arrays-part4/array.java
@@ -0,0 +1,32 @@
+class array{
+
+    //Longest Consecutive Sequence in an Array
+    public int longestConsecutive(int[] nums) {
+        int n = nums.length;
+
+        if(n==0)return 0;
+
+        int longest = 1;
+        Set<Integer> st = new HashSet<>();
+
+        for(int e:nums){
+            st.add(e);
+        }
+
+        for(int e:st){
+            if(!st.contains(e-1)){
+                int count = 1;
+                int elem = e;
+
+                while(st.contains(elem+1)){
+                    count+=1;
+                    elem=elem+1;
+                }
+
+                longest = Math.max(count,longest);
+            }
+        }
+
+        return longest;
+    }
+}
