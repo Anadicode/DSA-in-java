ce2677a1edbdab32bce1756a248e6b96d3276e65
Anadi <anirbanmondal53717@gmail.com>
2026-08-05 15:47:19 +0530

    114 Flatten linklist

--- Binary_Tree/Binary_Tree_Part1/BinaryTree.java
+++ Binary_Tree/Binary_Tree_Part1/BinaryTree.java
@@ -49,4 +49,22 @@ class BinaryTree{
                return root;
     }
 
+    // 114 Flatten linklist
+    private TreeNode prev=null;
+
+    public void flatten(TreeNode root) {
+        if(root==null)return ;
+
+
+        flatten(root.right);
+        flatten(root.left);
+
+        root.right=prev;
+        root.left=null;
+        prev=root;
+
+
+
+    }
+
 }
