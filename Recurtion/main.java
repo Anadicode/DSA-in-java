9a00389d9ce083418a00c7b249dfba1b6d402c35
Anadi <anirbanmondal53717@gmail.com>
2026-08-26 09:59:03 +0530

    46 parmutation

--- Recurtion/main.java
+++ Recurtion/main.java
@@ -27,6 +27,41 @@ class Solution {
     }
 }
 
+//46 Parmutation
+class Solution1 {
+
+    private void parmutation(int [ ] nums,boolean [] used,List<List<Integer>>ans,List<Integer>l){
+        if(l.size() == nums.length){
+            ans.add(new ArrayList<>(l));
+            return ;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(used[i])continue;
+
+            //pick
+            l.add(nums[i]);
+            used[i]=true;
+
+            //recurtion
+            parmutation(nums,used,ans,l);
+
+            // backtracking
+            l.remove(l.size()-1);
+            used[i]=false;
+
+        }
+    }
+
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> l = new ArrayList<>();
+        boolean [] used = new boolean[nums.length];
+        parmutation(nums,used,ans,l);
+        return ans;
+    }
+}
 
 public class Main{
 
