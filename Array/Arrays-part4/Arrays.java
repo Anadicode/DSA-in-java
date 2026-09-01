4206d57056a7a291cc7ce8c13dcd315ad60d4f2c
Anadi <anirbanmondal53717@gmail.com>
2026-09-01 23:47:55 +0530

    2 add two number

--- Array/Arrays-part4/Arrays.java
+++ Array/Arrays-part4/Arrays.java
@@ -59,4 +59,32 @@ class Arrays{
 
         return max;
     }
+
+
+
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode head = new ListNode(0);
+            ListNode temp = head;
+
+            int carry = 0;
+
+            while(l1!=null || l2!=null || carry!=0){
+                int n1 = (l1!=null)? l1.val : 0;
+                int n2 = (l2!=null) ? l2.val:0;
+
+                int sum = n1+n2+carry;
+                int rem = sum % 10;
+                carry = sum/10;
+                temp.next= new ListNode(rem);
+                temp=temp.next;
+
+                l1 = (l1!=null)? l1.next:null ;
+                l2 = (l2!=null)? l2.next:null ;
+            }
+
+            return head.next;
+        }
+
+
+
 }
